82fa11aca3e1b5802ac85c605f1403fd98930874
Praveen Sadhu <prsadhu@gmail.com>
2012-02-21 15:46:02 -0800

    Minor variable cleanup

--- src/main/java/com/netflix/priam/compress/SnappyCompression.java
+++ src/main/java/com/netflix/priam/compress/SnappyCompression.java
@@ -11,7 +11,7 @@ import org.apache.commons.io.IOUtils;
 import org.xerial.snappy.SnappyInputStream;
 
 /**
- * Class to generate compressed chunks of data from an input stream using 
+ * Class to generate compressed chunks of data from an input stream using
  * SnappyCompression
  */
 public class SnappyCompression implements ICompression
@@ -41,14 +41,14 @@ public class SnappyCompression implements ICompression
     private void decompress(InputStream input, OutputStream output) throws IOException
     {
         SnappyInputStream is = new SnappyInputStream(new BufferedInputStream(input));
-        byte d[] = new byte[BUFFER];
+        byte data[] = new byte[BUFFER];
         BufferedOutputStream dest1 = new BufferedOutputStream(output, BUFFER);
         try
         {
             int c;
-            while ((c = is.read(d, 0, BUFFER)) != -1)
+            while ((c = is.read(data, 0, BUFFER)) != -1)
             {
-                dest1.write(d, 0, c);
+                dest1.write(data, 0, c);
             }
         }
         finally

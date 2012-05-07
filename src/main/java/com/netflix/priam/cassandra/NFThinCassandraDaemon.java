c40aa617f5077c9644ec0ffe5fb36eb14e833f4c
Cody A. Ray <sites@codyaray.com>
2012-05-07 15:02:36 -0500

    NFThinCassandraDaemon should work with JSVC like Cassandra does

--- src/main/java/com/netflix/priam/cassandra/NFThinCassandraDaemon.java
+++ src/main/java/com/netflix/priam/cassandra/NFThinCassandraDaemon.java
@@ -1,5 +1,7 @@
 package com.netflix.priam.cassandra;
 
+import java.io.IOException;
+
 import org.apache.cassandra.thrift.CassandraDaemon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +12,36 @@ public class NFThinCassandraDaemon extends CassandraDaemon
 {
     private static final Logger logger = LoggerFactory.getLogger(NFThinCassandraDaemon.class);
 
+    /**
+     * Initialize the Cassandra Daemon based on the given command-line
+     * arguments. This is the normal {@code java} entry point.
+     * 
+     * @param args the arguments passed on the command-line
+     */
     public static void main(String[] args)
+    {
+        NFThinCassandraDaemon daemon = new NFThinCassandraDaemon();
+        daemon.setPriamProperties();
+        daemon.activate();
+    }
+
+    /**
+     * Initialize the Cassandra Daemon based on the given <a
+     * href="http://commons.apache.org/daemon/jsvc.html">Commons
+     * Daemon</a>-specific arguments. To clarify, this is a hook for
+     * JSVC and serves as a second entry point.
+     * 
+     * @param args the arguments passed in from JSVC
+     * @throws IOException
+     */
+    @Override
+    public void init(String[] args) throws IOException
+    {
+        setPriamProperties();
+        super.init(args);
+    }
+
+    private void setPriamProperties()
     {
         String token = null;
         String seeds = null;
@@ -27,7 +58,7 @@ public class NFThinCassandraDaemon extends CassandraDaemon
             {
                 logger.error("Failed to obtain a token from a pre-defined list, we can not start!", e);
             }
-
+  
             if (token != null && seeds != null)
                 break;
             // sleep for 5 sec and try again.
@@ -40,11 +71,10 @@ public class NFThinCassandraDaemon extends CassandraDaemon
                 // do nothing.
             }
         }
+        
         System.setProperty("cassandra.initial_token", token);
-
+  
         if (isReplace)
             System.setProperty("cassandra.replace_token", token);
-
-        new CassandraDaemon().activate();
     }
 }

e06f5ebca2d7b35f7570c7d3a8d6f40a0d9e6f7f
jason brown <jasedbrown@gmail.com>
2012-10-03 22:30:24 -0700

    Making a few minor changes to Aleksander's patch. Moved Application up to it's own file (and made public). Added S3FileSystem.shutdown() to it's interface, IBackupFileSystem. Up rev'ed the jmockit dependency version

--- priam/src/main/java/com/netflix/priam/cli/LightGuiceModule.java
+++ priam/src/main/java/com/netflix/priam/cli/LightGuiceModule.java
@@ -1,26 +1,22 @@
 package com.netflix.priam.cli;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-
 import com.netflix.priam.IConfiguration;
 import com.netflix.priam.ICredential;
-import com.netflix.priam.defaultimpl.PriamConfiguration;
-import com.netflix.priam.defaultimpl.ClearCredential;
 import com.netflix.priam.aws.S3BackupPath;
 import com.netflix.priam.aws.S3FileSystem;
 import com.netflix.priam.aws.SDBInstanceFactory;
-import com.netflix.priam.aws.S3FileSystem;
-import com.netflix.priam.identity.IMembership;
-import com.netflix.priam.identity.IPriamInstanceFactory;
 import com.netflix.priam.backup.AbstractBackupPath;
 import com.netflix.priam.backup.IBackupFileSystem;
 import com.netflix.priam.compress.ICompression;
 import com.netflix.priam.compress.SnappyCompression;
+import com.netflix.priam.defaultimpl.ClearCredential;
+import com.netflix.priam.defaultimpl.PriamConfiguration;
+import com.netflix.priam.identity.IMembership;
+import com.netflix.priam.identity.IPriamInstanceFactory;
+import com.netflix.priam.utils.ITokenManager;
 import com.netflix.priam.utils.Sleeper;
 import com.netflix.priam.utils.ThreadSleeper;
-import com.netflix.priam.utils.ITokenManager;
 import com.netflix.priam.utils.TokenManager;
 
 class LightGuiceModule extends AbstractModule
@@ -40,26 +36,3 @@ class LightGuiceModule extends AbstractModule
     }
 }
 
-class Application
-{
-    static Injector getInjector()
-    {
-        if (injector == null)
-            injector = Guice.createInjector(new LightGuiceModule());
-        return injector;
-    }
-
-    static void initialize()
-    {
-        IConfiguration conf = getInjector().getInstance(IConfiguration.class);
-        conf.intialize();
-    }
-
-    static void shutdownAdditionalThreads()
-    {
-        S3FileSystem fs = getInjector().getInstance(S3FileSystem.class);
-        fs.shutdown();
-    }
-
-    static private Injector injector;
-}

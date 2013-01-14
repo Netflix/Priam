0aff845620c506908b9f92d463d18f9a2dd8f5b7
jason brown <jasedbrown@gmail.com>
2013-01-14 13:32:48 -0800

    dse WIP: write out the c*/conf/cassandra-rackdc.properties with datacenter suffix

--- priam-dse-extensions/src/main/java/com/netflix/priam/dse/DseTuner.java
+++ priam-dse-extensions/src/main/java/com/netflix/priam/dse/DseTuner.java
@@ -1,9 +1,12 @@
 package com.netflix.priam.dse;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Map;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +14,11 @@ import org.slf4j.LoggerFactory;
 import com.google.inject.Inject;
 import com.netflix.priam.IConfiguration;
 import com.netflix.priam.defaultimpl.StandardTuner;
+import org.apache.cassandra.io.util.FileUtils;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import static com.netflix.priam.dse.IDseConfiguration.NodeType;
+import static org.apache.cassandra.locator.GossipingPropertyFileSnitch.RACKDC_PROPERTY_FILENAME;
 
 /**
  * Makes Datastax Enterprise-specific changes to the c* yaml and dse-yaml.
@@ -35,6 +41,7 @@ public class DseTuner extends StandardTuner
     {
         super.updateYaml(yamlLocation, hostname, seedProvider);
         writeDseYaml();
+        writeCassandraSnitchProperties();
     }
 
     private void writeDseYaml() throws IOException
@@ -48,6 +55,38 @@ public class DseTuner extends StandardTuner
         map.put("delegated_snitch", config.getSnitch());
         logger.info("Updating dse-yaml: " + yaml.dump(map));
         yaml.dump(map, new FileWriter(dseYaml));
+    }
+
+    private void writeCassandraSnitchProperties()
+    {
+        final NodeType nodeType = dseConfig.getNodeType();
+        if(nodeType == NodeType.REAL_TIME_QUERY)
+            return;
+
+        Reader reader = null;
+        try
+        {
+            String filePath = config.getCassHome() + "/conf/" + RACKDC_PROPERTY_FILENAME;
+            reader = new FileReader(filePath);
+            Properties properties = new Properties();
+            properties.load(reader);
+            String suffix = "";
+            if(nodeType == NodeType.SEARCH)
+                suffix = "_solr";
+            if(nodeType == NodeType.ANALYTIC)
+                suffix = "_hadoop";
+            properties.put("dc_suffix", suffix);
+            properties.store(new FileWriter(filePath), "");
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Unable to read " + RACKDC_PROPERTY_FILENAME, e);
+        }
+        finally
+        {
+            FileUtils.closeQuietly(reader);
+        }
+
     }
 
     protected String getSnitch()

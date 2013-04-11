eaa5cd449a75239726a9c59b587aeabbfc4d6840
Jason Brown <jasedbrown@gmail.com>
2013-04-11 16:05:12 -0700

    backporting fix for writing auth values to yaml. Also fixed the 1.1 yaml name 'authority' to 'authorizer' (changed in 1.2)

--- priam/src/main/java/com/netflix/priam/defaultimpl/StandardTuner.java
+++ priam/src/main/java/com/netflix/priam/defaultimpl/StandardTuner.java
@@ -62,7 +62,7 @@ public class StandardTuner implements CassandraTuner
         map.put("max_hint_window_in_ms", config.getMaxHintWindowInMS());
         map.put("hinted_handoff_throttle_delay_in_ms", config.getHintHandoffDelay());
         map.put("authenticator", config.getAuthenticator());
-        map.put("authority", config.getAuthorizer());
+        map.put("authorizer", config.getAuthorizer());
 
         List<?> seedp = (List) map.get("seed_provider");
         Map<String, String> m = (Map<String, String>) seedp.get(0);

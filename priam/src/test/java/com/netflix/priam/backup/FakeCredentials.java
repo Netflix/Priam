cecdd979a687c4006d1bdb13d96f3f1c0abb9290
Jason Brown <jasedbrown@gmail.com>
2013-12-09 13:25:18 -0800

    cleaned up ICredential. #305

--- priam/src/test/java/com/netflix/priam/backup/FakeCredentials.java
+++ priam/src/test/java/com/netflix/priam/backup/FakeCredentials.java
@@ -1,31 +1,10 @@
 package com.netflix.priam.backup;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.netflix.priam.ICredential;
 
 public class FakeCredentials implements ICredential
 {
-
-    @Override
-    public String getAccessKeyId()
-    {
-        return "";
-    }
-
-    @Override
-    public String getSecretAccessKey()
-    {
-        return "";
-    }
-
-    public AWSCredentials getCredentials()
-    {
-        return new BasicAWSCredentials(getAccessKeyId(), getSecretAccessKey());
-    }
-
-	@Override
 	public AWSCredentialsProvider getAwsCredentialProvider() {
 		// TODO Auto-generated method stub
 		return null;

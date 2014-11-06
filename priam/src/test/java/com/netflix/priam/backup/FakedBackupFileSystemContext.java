6dfcc2a2815bb433806eea71ae1d48b4c8d1337d
vinh.nguyen <vinhn@netflix.com>
2014-11-05 16:42:42 -0800

    Attempting to resolve InstanceAlreadyExist error for unit test by creating my own fake classes and binding it within the Guice module used by the unit tests

--- priam/src/test/java/com/netflix/priam/backup/FakedBackupFileSystemContext.java
+++ priam/src/test/java/com/netflix/priam/backup/FakedBackupFileSystemContext.java
@@ -1,15 +1,23 @@
 package com.netflix.priam.backup;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 import com.netflix.priam.IConfiguration;
 
 @Singleton
 public class FakedBackupFileSystemContext implements IFileSystemContext {
 
+	private IBackupFileSystem fs;
+
+	@Inject
+	public FakedBackupFileSystemContext(@Named("backup") IBackupFileSystem fs) {
+		this.fs = fs;
+	}
+	
 	@Override
 	public IBackupFileSystem getFileStrategy(IConfiguration config) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.fs;
 	}
 
 }

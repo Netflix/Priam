f8bdf401def2fd90635dfa8d1bad900ee2dbfd2c
timiblossom <minh.n.do@gmail.com>
2015-06-02 23:14:28 -0700

    Use token instead of status field for correctness and simplicity

--- priam/src/main/java/com/netflix/priam/identity/token/DeadTokenRetriever.java
+++ priam/src/main/java/com/netflix/priam/identity/token/DeadTokenRetriever.java
@@ -145,11 +145,10 @@ public class DeadTokenRetriever extends TokenRetrieverBase implements IDeadToken
                 if (msg.get("Token") == null) {
                     continue;
                 }
-	            String statusVal = (String) msg.get("Token");
-                String[] ss = statusVal.split(",");
-                logger.info("Using gossipinfo from host[" + host + "] and token[" + token + "], the replaced address is : " + key);
-            
-                if (ss[1].equals(token)) {
+	            String tokenVal = (String) msg.get("Token");
+
+                if (token.equals(tokenVal)) {
+                   logger.info("Using gossipinfo from host[" + host + "] and token[" + token + "], the replaced address is : " + key);                   
                    return (String) key;
                 }
             

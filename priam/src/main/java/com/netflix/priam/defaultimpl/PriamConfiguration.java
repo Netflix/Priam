c4e49756b57097c9b89c6ce2f38ff6aaf2b3edb6
Ryan Bourgeois <bluedragonx@gmail.com>
2016-08-01 14:42:42 -0700

    Fix PriamConfiguration init order.

--- priam/src/main/java/com/netflix/priam/defaultimpl/PriamConfiguration.java
+++ priam/src/main/java/com/netflix/priam/defaultimpl/PriamConfiguration.java
@@ -275,15 +275,6 @@ public class PriamConfiguration implements IConfiguration
     @Override
     public void intialize()
     {
-        setupEnvVars();
-        this.config.intialize(ASG_NAME, REGION);
-        setDefaultRACList(REGION);
-        populateProps();
-        SystemUtils.createDirs(getBackupCommitLogLocation());
-        SystemUtils.createDirs(getCommitLogLocation());
-        SystemUtils.createDirs(getCacheLocation());
-        SystemUtils.createDirs(getDataFileLocation());
-        
     	InstanceDataRetriever instanceDataRetriever;
 		try {
 			instanceDataRetriever = getInstanceDataRetriever();
@@ -300,6 +291,15 @@ public class PriamConfiguration implements IConfiguration
 
 		NETWORK_MAC =  instanceDataRetriever.getMac();
 		NETWORK_VPC = instanceDataRetriever.getVpcId();
+
+        setupEnvVars();
+        this.config.intialize(ASG_NAME, REGION);
+        setDefaultRACList(REGION);
+        populateProps();
+        SystemUtils.createDirs(getBackupCommitLogLocation());
+        SystemUtils.createDirs(getCommitLogLocation());
+        SystemUtils.createDirs(getCacheLocation());
+        SystemUtils.createDirs(getDataFileLocation());
     }
     
     private InstanceDataRetriever getInstanceDataRetriever() throws InstantiationException, IllegalAccessException, ClassNotFoundException

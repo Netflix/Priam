3f27d6ac6441732fe6087a4dddc0956db0266887
Arun Agrawal <aagrawal@netflix.com>
2017-02-27 13:32:02 -0800

    Refactor: BackupStatusMgr

--- priam/src/main/java/com/netflix/priam/backup/IBackupStatusMgr.java
+++ priam/src/main/java/com/netflix/priam/backup/IBackupStatusMgr.java
@@ -1,11 +1,14 @@
 package com.netflix.priam.backup;
 
+import com.google.inject.ImplementedBy;
+
 import java.util.Date;
 import java.util.List;
 
 /**
  * Created by aagrawal on 1/30/17.
  */
+@ImplementedBy(BackupStatusMgr.class)
 public interface IBackupStatusMgr {
 
     public void add(IMessageObserver.BACKUP_MESSAGE_TYPE message_type, String backup, Date startTime, Date completedTime);

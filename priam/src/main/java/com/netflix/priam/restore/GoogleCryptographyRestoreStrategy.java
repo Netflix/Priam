b1ba2141f9f8ea4f99c248c730efcd0f5547b80e
Arun Agrawal <aagrawal@netflix.com>
2017-10-01 10:40:07 -0700

    Remove duplicated code among all restore techniques.

--- priam/src/main/java/com/netflix/priam/restore/GoogleCryptographyRestoreStrategy.java
+++ priam/src/main/java/com/netflix/priam/restore/GoogleCryptographyRestoreStrategy.java
@@ -15,8 +15,6 @@
  */
 package com.netflix.priam.restore;
 
-import com.google.common.collect.Iterators;
-import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
@@ -24,10 +22,7 @@ import com.google.inject.name.Named;
 import com.netflix.priam.ICassandraProcess;
 import com.netflix.priam.IConfiguration;
 import com.netflix.priam.ICredentialGeneric;
-import com.netflix.priam.ICredentialGeneric.KEY;
-import com.netflix.priam.aws.S3BackupPath;
 import com.netflix.priam.backup.AbstractBackupPath;
-import com.netflix.priam.backup.AbstractBackupPath.BackupFileType;
 import com.netflix.priam.backup.IBackupFileSystem;
 import com.netflix.priam.backup.MetaData;
 import com.netflix.priam.backup.RestoreTokenSelector;
@@ -36,15 +31,9 @@ import com.netflix.priam.cryptography.IFileCryptography;
 import com.netflix.priam.identity.InstanceIdentity;
 import com.netflix.priam.scheduler.SimpleTimer;
 import com.netflix.priam.scheduler.TaskTimer;
-import com.netflix.priam.utils.RetryableCallable;
 import com.netflix.priam.utils.Sleeper;
-import com.netflix.priam.utils.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.math.BigInteger;
-import java.util.*;
 
 @Singleton
 public class GoogleCryptographyRestoreStrategy extends RestoreBase implements IRestoreStrategy {
@@ -52,180 +41,17 @@ public class GoogleCryptographyRestoreStrategy extends RestoreBase implements IR
     public static final String JOBNAME = "GOOGLECLOUDSTORAGE_RESTORE_JOB";
 
     @Inject
-    private Provider<AbstractBackupPath> pathProvider;
-    @Inject
-    private MetaData metaData;
-    @Inject
-    private InstanceIdentity id;
-    @Inject
-    private RestoreTokenSelector tokenSelector;
-
-    private ICassandraProcess cassProcess;
-    private IFileCryptography fileCryptography;
-    private ICompression compress;
-    private ICredentialGeneric pgpCredential;
-
-    @Inject
     public GoogleCryptographyRestoreStrategy(final IConfiguration config, ICassandraProcess cassProcess, @Named("gcsencryptedbackup") IBackupFileSystem fs, Sleeper sleeper
             , @Named("filecryptoalgorithm") IFileCryptography fileCryptography
             , @Named("pgpcredential") ICredentialGeneric credential
-            , ICompression compress
+            , ICompression compress, Provider<AbstractBackupPath> pathProvider,
+                                             InstanceIdentity id, RestoreTokenSelector tokenSelector, MetaData metaData
     ) {
-
-        super(config, fs, JOBNAME, sleeper);
-        this.cassProcess = cassProcess;
-        this.fileCryptography = fileCryptography;
-        this.pgpCredential = credential;
-        this.compress = compress;
-    }
-
-    @Override
-    public void execute() throws Exception {
-
-        if (isRestoreEnabled(config)) {
-            logger.info("Starting restore for " + config.getRestoreSnapshot()); //get the date range
-            String[] restore = config.getRestoreSnapshot().split(",");
-            AbstractBackupPath path = pathProvider.get();
-            final Date startTime = path.parseDate(restore[0]);
-            final Date endTime = path.parseDate(restore[1]);
-            String origToken = id.getInstance().getToken();
-            try {
-                if (config.isRestoreClosestToken()) {
-                    restoreToken = tokenSelector.getClosestToken(new BigInteger(origToken), startTime);
-                    id.getInstance().setToken(restoreToken.toString());
-                }
-                new RetryableCallable<Void>() {
-                    public Void retriableCall() throws Exception {
-                        logger.info("Attempting restore");
-                        restore(startTime, endTime);
-                        logger.info("Restore completed");
-                        // Wait for other server init to complete
-                        sleeper.sleep(30000);
-                        return null;
-                    }
-                }.call();
-            } finally {
-                id.getInstance().setToken(origToken);
-            }
-        }
-        cassProcess.start(true);
-
-    }
-
-    @Override
-    public void restore(Date startTime, Date endTime) throws Exception {
-
-        super.stopCassProcess(this.cassProcess);        // Stop cassandra if its running and restoring all keyspaces
-
-        //== Cleanup local data
-        SystemUtils.cleanupDir(super.config.getDataFileLocation(), super.config.getRestoreKeySpaces());
-
-        //== Generate Json format list of all files to download. This information is derived from meta.json file.
-        List<AbstractBackupPath> metas = Lists.newArrayList();
-
-        fetchSnapshotMetaFile(getRestorePrefix(), metas, startTime, endTime);
-        if (metas.size() == 0) {
-            logger.error("No snapshot meta file found, Restore Failed.");
-            assert false : "[GoogleRestoreStrategy] No snapshots meta data file found, Restore Failed.";
-            return;
-        }
-
-        Collections.sort(metas);
-        AbstractBackupPath meta = Iterators.getLast(metas.iterator());
-        logger.info("Snapshot Meta file for restore " + meta.getRemotePath());
-
-        //download, decrypt, and uncompress the metadata file
-        List<AbstractBackupPath> metaFile = new ArrayList<AbstractBackupPath>();
-        metaFile.add(meta);
-        performDownloadType(metaFile.iterator(), BackupFileType.META);
-
-        List<AbstractBackupPath> snapshots = metaData.toJson(meta.newRestoreFile()); //transform the decompress, decrypted meta file to json format
-
-        // Download the Snapshots, decrypt, and then decompress
-        performDownloadType(snapshots.iterator(), BackupFileType.SNAP);
-
-        logger.info("Downloading incrementals");
-        // Download incrementals (SST).
-        Iterator<AbstractBackupPath> incrementals = fs.list(getRestorePrefix(), meta.getTime(), endTime);
-        performDownloadType(incrementals, BackupFileType.SST);
-
-        //Downloading CommitLogs
-        if (config.isBackingUpCommitLogs()) {
-            logger.info("Delete all backuped commitlog files in " + config.getBackupCommitLogLocation());
-            SystemUtils.cleanupDir(config.getBackupCommitLogLocation(), null);
-
-            logger.info("Delete all commitlog files in " + config.getCommitLogLocation());
-            SystemUtils.cleanupDir(config.getCommitLogLocation(), null);
-
-            Iterator<AbstractBackupPath> commitLogPathIterator = fs.list(getRestorePrefix(), meta.getTime(), endTime);
-            download(commitLogPathIterator, BackupFileType.CL, config.maxCommitLogsRestore());
-        }
-
-    }
-
-    /*
-     * A version of download commit logs, base on AbstractRestore.download(Iterator<AbstractBackupPath> fsIterator, BackupFileType filter, int lastN).
-     * This version does not change the original business logic, it just assumes the commit logs are encrypted and handles it.
-    */
-    @Override
-    protected void download(Iterator<AbstractBackupPath> fsIterator, BackupFileType filter, int lastN) throws Exception {
-        if (fsIterator == null)
-            return;
-
-        BoundedList bl = new BoundedList(lastN);
-        while (fsIterator.hasNext()) {
-            AbstractBackupPath temp = fsIterator.next();
-            if (temp.getType() == BackupFileType.SST && tracker.contains(temp))
-                continue;
-
-            if (temp.getType() == filter) {
-                bl.add(temp);
-            }
-        }
-
-        performDownloadType(bl.iterator(), filter);
+        super(config, fs, JOBNAME, sleeper, cassProcess, pathProvider, id, tokenSelector, credential, fileCryptography, compress, metaData);
     }
 
 
-    /*
-     * Determines the type of download (data is encrypted or not) for the objects.
-     *
-     * @param fsIterator - a list of objects to download.
-     * @param filer - type (e.g. SNAP, SST) of objects to download.
-     * @param decryptAndDecompress - true to decrypt the download object on disk and then decompress it..
-     */
-    private void performDownloadType(Iterator<AbstractBackupPath> fsIterator, BackupFileType filter) throws Exception {
-        if (!this.config.isEncryptBackupEnabled()) {
-
-            download(fsIterator, filter);
-
-        } else {
-
-            //download & decrypt each object
-            while (fsIterator.hasNext()) {
-                AbstractBackupPath temp = fsIterator.next();
-                if (temp.getType().equals(BackupFileType.SST) && tracker.contains(temp))
-                    continue;
-
-                if (temp.getType() == filter) {
-                    File localFileHandler = temp.newRestoreFile();
-                    String tempFileName = localFileHandler.getAbsolutePath() + ".tmp";
-                    File tempFileHandler = new File(tempFileName);
-
-                    //download from source, decrypt, and lastly uncompress
-
-                    //download(temp, localFileHandler, encryptedFileHandler, this.fileCryptography, this.keyCryptography.decrypt(null).toCharArray(), this.compress);
-                    String pgpPassword = new String(this.pgpCredential.getValue(KEY.PGP_PASSWORD));
-                    download(temp, localFileHandler, tempFileHandler, this.fileCryptography, pgpPassword.toCharArray(), this.compress);
-                }
-            }
-
-            waitToComplete(); //wait until all objects are downloaded, decrypt, and uncompress
-
-        }
-    }
-
-    /*
+    /**
      * @return a timer used by the scheduler to determine when "this" should be run.
      */
     public static TaskTimer getTimer() {
@@ -233,15 +59,6 @@ public class GoogleCryptographyRestoreStrategy extends RestoreBase implements IR
     }
 
 
-    /*
-     * @param pathprefix - the absolute path (including bucket name) to the object.
-     * @param bucket name
-     */
-    public static String getSourcebucket(String pathPrefix) {
 
-        String[] paths = pathPrefix.split(String.valueOf(S3BackupPath.PATH_SEP));
-        return paths[0];
-
-    }
 
 }

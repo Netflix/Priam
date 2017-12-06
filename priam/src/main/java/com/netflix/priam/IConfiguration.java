0cf5fb4e90de11c4a46db02653e919ef9c9b2c4b
Joseph Lynch <josephl@netflix.com>
2017-12-06 15:25:40 -0800

    Docstring typo and adding changelog

--- priam/src/main/java/com/netflix/priam/IConfiguration.java
+++ priam/src/main/java/com/netflix/priam/IConfiguration.java
@@ -51,7 +51,7 @@ public interface IConfiguration {
     public String getCassStopScript();
 
     /**
-     * @return double representing how often (in seconds) Priam should auto-remediate Cassandra process crash
+     * @return int representing how often (in seconds) Priam should auto-remediate Cassandra process crash
      * If zero, Priam will restart Cassandra whenever it notices it is crashed
      * If a positive number, Priam will restart cassandra no more than once in that number of seconds. For example a
      * value of 60 means that Priam will only restart Cassandra once per 60 seconds

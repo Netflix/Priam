e3538548937509a1548b71052c737a2174c1266c
Joseph Lynch <josephl@netflix.com>
2018-08-30 18:39:02 -0700

    Remove deprecated cassandra.yaml options from the StandardTuner

--- priam/src/main/java/com/netflix/priam/tuner/StandardTuner.java
+++ priam/src/main/java/com/netflix/priam/tuner/StandardTuner.java
@@ -49,12 +49,10 @@ public class StandardTuner implements ICassandraTuner {
         options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
         Yaml yaml = new Yaml(options);
         File yamlFile = new File(yamlLocation);
-        Map map = (Map) yaml.load(new FileInputStream(yamlFile));
+        Map map = yaml.load(new FileInputStream(yamlFile));
         map.put("cluster_name", config.getAppName());
         map.put("storage_port", config.getStoragePort());
         map.put("ssl_storage_port", config.getSSLStoragePort());
-        map.put("start_rpc", config.isThriftEnabled());
-        map.put("rpc_port", config.getThriftPort());
         map.put("start_native_transport", config.isNativeTransportEnabled());
         map.put("native_transport_port", config.getNativeTransportPort());
         map.put("listen_address", hostname);
@@ -101,9 +99,6 @@ public class StandardTuner implements ICassandraTuner {
         map.put("concurrent_writes", config.getConcurrentWritesCnt());
         map.put("concurrent_compactors", config.getConcurrentCompactorsCnt());
 
-        map.put("rpc_server_type", config.getRpcServerType());
-        map.put("rpc_min_threads", config.getRpcMinThreads());
-        map.put("rpc_max_threads", config.getRpcMaxThreads());
         // Add private ip address as broadcast_rpc_address. This will ensure that COPY function works correctly. 
         map.put("broadcast_rpc_address", config.getInstanceDataRetriever().getPrivateIP());
         //map.put("index_interval", config.getIndexInterval());

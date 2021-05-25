Netflix/Priam

commit a48d1b7d2bf02fbaaa9028e03970cd5f9233c255

Skip backup compression based on configuration (#922)
* CASS-1264 remove dead code.

* CASS-1264 Move buffer size constraint to the only place it is used and simplify its usage.

* CASS-1264 simplify getChunkSize()

* CASS-1264 style tweaks

* CASS-1264 remove redundant comments

* CASS-1264 change IBackupFileSystem download api to include compression information

* CASS-1264 pushing AbstractBackupPath (read compression info) further down. Plus change the getFileSize method to take a String to better accommodate existing usage patterns.

* CASS-1264 remove option to delete file after upload. It is always deleted in practice.

* CASS-1264 remove redundant Path parameters from upload api. They are embedded in the AbstractBackupPath in practice.

* CASS-1264 push compression info all the way down to where it is needed.

* CASS-1264 skip uncompression on download.

* CASS-1264 Skip compression on backup based on desires in path.

* CASS-1264 choose compression behavior on backup based on fast property rather tahn just defaulting to Snappy always.

* CASS-1264 a little reorganization in advance of making metafiles aware of varying compression.

* CASS-1264 Make metafile display compression algorithm properly

* CASS-1264 Limit conditional compression to V2 backups.

* CASS-1264 Addressing review comments.

* CASS-1264 ensure disctinction between creation time and last modified in FileUploadResult. They may not always be the same in practice.

* CASS-1264 rename BackupsToCompress.UNCOMPRESSED to IF_REQUIRED per review feedback

* CASS-1264 Rename CompressionAlgorithm enum to CompressionType per review comments

priam/src/main/java/com/netflix/priam/backup/AbstractBackup.java
@@ -16,19 +16,24 @@
  */
 package com.netflix.priam.backup;
 
+import static java.util.stream.Collectors.toSet;
+
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.netflix.priam.backup.AbstractBackupPath.BackupFileType;
+import com.netflix.priam.compress.CompressionType;
+import com.netflix.priam.config.BackupsToCompress;
 import com.netflix.priam.config.IConfiguration;
 import com.netflix.priam.scheduler.Task;
 import com.netflix.priam.utils.SystemUtils;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.ParseException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -39,6 +44,7 @@ import org.slf4j.LoggerFactory;
 /** Abstract Backup class for uploading files to backup location */
 public abstract class AbstractBackup extends Task {
     private static final Logger logger = LoggerFactory.getLogger(AbstractBackup.class);
+    private static final String COMPRESSION_SUFFIX = "-CompressionInfo.db";
     static final String INCREMENTAL_BACKUP_FOLDER = "backups";
     public static final String SNAPSHOT_FOLDER = "snapshots";
 
@@ -56,18 +62,6 @@ public abstract class AbstractBackup extends Task {
         this.fs = backupFileSystemCtx.getFileStrategy(config);
     }
 
-    /** A means to override the type of backup strategy chosen via BackupFileSystemContext */
-    protected void setFileSystem(IBackupFileSystem fs) {
-        this.fs = fs;
-    }
-
-    private AbstractBackupPath getAbstractBackupPath(final File file, final BackupFileType type)
-            throws ParseException {
-        final AbstractBackupPath bp = pathFactory.get();
-        bp.parseLocal(file, type);
-        return bp;
-    }
-
     /**
      * Upload files in the specified dir. Does not delete the file in case of error. The files are
      * uploaded serially or async based on flag provided.
@@ -80,46 +74,65 @@ public abstract class AbstractBackup extends Task {
      * @return List of files that are successfully uploaded as part of backup
      * @throws Exception when there is failure in uploading files.
      */
-    protected List<AbstractBackupPath> upload(
+    protected ImmutableSet<AbstractBackupPath> upload(
             final File parent, final BackupFileType type, boolean async, boolean waitForCompletion)
             throws Exception {
-        final List<AbstractBackupPath> bps = Lists.newArrayList();
-        final List<Future<Path>> futures = Lists.newArrayList();
-
-        File[] files = parent.listFiles();
-        if (files == null) return bps;
-
-        for (File file : files) {
-            if (file.isFile() && file.exists()) {
-                AbstractBackupPath bp = getAbstractBackupPath(file, type);
-
-                if (async)
-                    futures.add(
-                            fs.asyncUploadFile(
-                                    Paths.get(bp.getBackupFile().getAbsolutePath()),
-                                    Paths.get(bp.getRemotePath()),
-                                    bp,
-                                    10,
-                                    true));
-                else
-                    fs.uploadFile(
-                            Paths.get(bp.getBackupFile().getAbsolutePath()),
-                            Paths.get(bp.getRemotePath()),
-                            bp,
-                            10,
-                            true);
-
-                bps.add(bp);
-            }
+        ImmutableSet<AbstractBackupPath> bps = getBackupPaths(parent, type);
+        final List<Future<AbstractBackupPath>> futures = Lists.newArrayList();
+        for (AbstractBackupPath bp : bps) {
+            if (async) futures.add(fs.asyncUploadAndDelete(bp, 10));
+            else fs.uploadAndDelete(bp, 10);
         }
 
         // Wait for all files to be uploaded.
         if (async && waitForCompletion) {
-            for (Future future : futures)
+            for (Future<AbstractBackupPath> future : futures)
                 future.get(); // This might throw exception if there is any error
         }
 
         return bps;
+    }
+
+    protected ImmutableSet<AbstractBackupPath> getBackupPaths(File dir, BackupFileType type)
+            throws IOException {
+        Set<File> files =
+                Files.list(dir.toPath()).map(Path::toFile).filter(File::isFile).collect(toSet());
+        Set<String> compressedFilePrefixes =
+                files.stream()
+                        .map(File::getName)
+                        .filter(name -> name.endsWith(COMPRESSION_SUFFIX))
+                        .map(name -> name.substring(0, name.lastIndexOf('-')))
+                        .collect(toSet());
+        final ImmutableSet.Builder<AbstractBackupPath> bps = ImmutableSet.builder();
+        for (File file : files) {
+            final AbstractBackupPath bp = pathFactory.get();
+            bp.parseLocal(file, type);
+            bp.setCompression(getCorrectCompressionAlgorithm(bp, compressedFilePrefixes));
+            bps.add(bp);
+        }
+        return bps.build();
+    }
+
+    private CompressionType getCorrectCompressionAlgorithm(
+            AbstractBackupPath path, Set<String> compressedFiles) {
+        if (!BackupFileType.isV2(path.getType())) {
+            return CompressionType.SNAPPY;
+        }
+        String file = path.getFileName();
+        BackupsToCompress which = config.getBackupsToCompress();
+        switch (which) {
+            case NONE:
+                return CompressionType.NONE;
+            case ALL:
+                return CompressionType.SNAPPY;
+            case IF_REQUIRED:
+                int splitIndex = file.lastIndexOf('-');
+                return splitIndex >= 0 && compressedFiles.contains(file.substring(0, splitIndex))
+                        ? CompressionType.NONE
+                        : CompressionType.SNAPPY;
+            default:
+                throw new IllegalArgumentException("NONE, ALL, UNCOMPRESSED only. Saw: " + which);
+        }
     }
 
     protected final void initiateBackup(

priam/src/main/java/com/netflix/priam/aws/RemoteBackupPath.java
@@ -16,11 +16,13 @@
  */
 package com.netflix.priam.aws;
 
+import com.google.api.client.util.Lists;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import com.netflix.priam.backup.AbstractBackupPath;
+import com.netflix.priam.compress.CompressionType;
 import com.netflix.priam.config.IConfiguration;
 import com.netflix.priam.identity.InstanceIdentity;
 import com.netflix.priam.utils.DateUtil;
@@ -29,6 +31,7 @@ import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -102,16 +105,23 @@ public class RemoteBackupPath extends AbstractBackupPath {
         type = BackupFileType.valueOf(remotePath.getName(index++).toString());
         String lastModified = remotePath.getName(index++).toString();
         setLastModified(Instant.ofEpochMilli(Long.parseLong(lastModified)));
+        List<String> parts = Lists.newArrayListWithCapacity(4);
         if (BackupFileType.isDataFile(type)) {
             keyspace = remotePath.getName(index++).toString();
             columnFamily = remotePath.getName(index++).toString();
+            parts.add(keyspace);
+            parts.add(columnFamily);
         }
         if (type == BackupFileType.SECONDARY_INDEX_V2) {
             indexDir = remotePath.getName(index++).toString();
+            parts.add(indexDir);
         }
-        setCompression(remotePath.getName(index++).toString());
+        setCompression(CompressionType.valueOf(remotePath.getName(index++).toString()));
         setEncryption(remotePath.getName(index++).toString());
         fileName = remotePath.getName(index).toString();
+        parts.add(fileName);
+        this.backupFile =
+                Paths.get(config.getDataFileLocation(), parts.toArray(new String[] {})).toFile();
     }
 
     private String getV1Location() {

priam/src/main/java/com/netflix/priam/aws/S3EncryptedFileSystem.java
@@ -26,6 +26,7 @@ import com.google.inject.name.Named;
 import com.netflix.priam.backup.AbstractBackupPath;
 import com.netflix.priam.backup.BackupRestoreException;
 import com.netflix.priam.backup.RangeReadInputStream;
+import com.netflix.priam.compress.ChunkedStream;
 import com.netflix.priam.compress.ICompression;
 import com.netflix.priam.config.IConfiguration;
 import com.netflix.priam.cred.ICredential;
@@ -35,6 +36,7 @@ import com.netflix.priam.merics.BackupMetrics;
 import com.netflix.priam.notification.BackupNotificationMgr;
 import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 import org.apache.commons.io.IOUtils;
@@ -69,14 +71,14 @@ public class S3EncryptedFileSystem extends S3FileSystemBase {
     }
 
     @Override
-    protected void downloadFileImpl(Path remotePath, Path localPath) throws BackupRestoreException {
+    protected void downloadFileImpl(AbstractBackupPath path, String suffix)
+            throws BackupRestoreException {
+        String remotePath = path.getRemotePath();
+        Path localPath = Paths.get(path.newRestoreFile().getAbsolutePath() + suffix);
         try (OutputStream os = new FileOutputStream(localPath.toFile());
                 RangeReadInputStream rris =
                         new RangeReadInputStream(
-                                s3Client,
-                                getShard(),
-                                super.getFileSize(remotePath),
-                                remotePath.toString())) {
+                                s3Client, getShard(), super.getFileSize(remotePath), remotePath)) {
             /*
              * To handle use cases where decompression should be done outside of the download.  For example, the file have been compressed and then encrypted.
              * Hence, decompressing it here would compromise the decryption.
@@ -95,18 +97,18 @@ public class S3EncryptedFileSystem extends S3FileSystemBase {
     }
 
     @Override
-    protected long uploadFileImpl(Path localPath, Path remotePath) throws BackupRestoreException {
+    protected long uploadFileImpl(AbstractBackupPath path) throws BackupRestoreException {
+        Path localPath = Paths.get(path.getBackupFile().getAbsolutePath());
+        String remotePath = path.getRemotePath();
+
         long chunkSize = getChunkSize(localPath);
         // initialize chunking request to aws
         InitiateMultipartUploadRequest initRequest =
-                new InitiateMultipartUploadRequest(config.getBackupPrefix(), remotePath.toString());
+                new InitiateMultipartUploadRequest(config.getBackupPrefix(), remotePath);
         // Fetch the aws generated upload id for this chunking request
         InitiateMultipartUploadResult initResponse = s3Client.initiateMultipartUpload(initRequest);
         DataPart part =
-                new DataPart(
-                        config.getBackupPrefix(),
-                        remotePath.toString(),
-                        initResponse.getUploadId());
+                new DataPart(config.getBackupPrefix(), remotePath, initResponse.getUploadId());
         // Metadata on number of parts to be uploaded
         List<PartETag> partETags = Lists.newArrayList();
 
@@ -121,7 +123,8 @@ public class S3EncryptedFileSystem extends S3FileSystemBase {
         try (InputStream in = new FileInputStream(localPath.toFile());
                 BufferedOutputStream compressedBos =
                         new BufferedOutputStream(new FileOutputStream(compressedDstFile))) {
-            Iterator<byte[]> compressedChunks = this.compress.compress(in, chunkSize);
+            Iterator<byte[]> compressedChunks =
+                    new ChunkedStream(in, chunkSize, path.getCompression());
             while (compressedChunks.hasNext()) {
                 byte[] compressedChunk = compressedChunks.next();
                 compressedBos.write(compressedChunk);
@@ -137,8 +140,7 @@ public class S3EncryptedFileSystem extends S3FileSystemBase {
         // == Read compressed data, encrypt each chunk, upload it to aws
         try (BufferedInputStream compressedBis =
                 new BufferedInputStream(new FileInputStream(compressedDstFile))) {
-            Iterator<byte[]> chunks =
-                    this.encryptor.encryptStream(compressedBis, remotePath.toString());
+            Iterator<byte[]> chunks = this.encryptor.encryptStream(compressedBis, remotePath);
 
             // identifies this part position in the object we are uploading
             int partNum = 0;
@@ -154,7 +156,7 @@ public class S3EncryptedFileSystem extends S3FileSystemBase {
                                 ++partNum,
                                 chunk,
                                 config.getBackupPrefix(),
-                                remotePath.toString(),
+                                remotePath,
                                 initResponse.getUploadId());
                 S3PartUploader partUploader = new S3PartUploader(s3Client, dp, partETags);
                 encryptedFileSize += chunk.length;

commit f4dba87b38614d0d69e4d6b680a4e50734193e2d

Fix file descriptor leak introduced in PR #922

priam/src/main/java/com/netflix/priam/backup/AbstractBackup.java
@@ -38,6 +38,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Future;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -95,8 +96,10 @@ public abstract class AbstractBackup extends Task {
 
     protected ImmutableSet<AbstractBackupPath> getBackupPaths(File dir, BackupFileType type)
             throws IOException {
-        Set<File> files =
-                Files.list(dir.toPath()).map(Path::toFile).filter(File::isFile).collect(toSet());
+        Set<File> files;
+        try (Stream<Path> pathStream = Files.list(dir.toPath())) {
+            files = pathStream.map(Path::toFile).filter(File::isFile).collect(toSet());
+        }
         Set<String> compressedFilePrefixes =
                 files.stream()
                         .map(File::getName)
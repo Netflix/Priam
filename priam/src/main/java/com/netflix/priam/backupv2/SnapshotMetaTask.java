a4e0c7ec1cf51127f8a4dcc36a413b16da5a9536
mattl-netflix <63665634+mattl-netflix@users.noreply.github.com>
2023-01-18 15:21:21 -0800

    Log backup failures rather than ignoring them. (#1025)

--- priam/src/main/java/com/netflix/priam/backupv2/SnapshotMetaTask.java
+++ priam/src/main/java/com/netflix/priam/backupv2/SnapshotMetaTask.java
@@ -19,8 +19,10 @@ package com.netflix.priam.backupv2;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSetMultimap;
+import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.netflix.priam.backup.*;
 import com.netflix.priam.config.IBackupRestoreConfig;
 import com.netflix.priam.config.IConfiguration;
@@ -285,7 +287,9 @@ public class SnapshotMetaTask extends AbstractBackup {
                 // We do not want to wait for completion and we just want to add them to queue. This
                 // is to ensure that next run happens on time.
                 AbstractBackupPath.BackupFileType type = AbstractBackupPath.BackupFileType.SST_V2;
-                backupHelper.uploadAndDeleteAllFiles(snapshotDirectory, type, target, true);
+                backupHelper
+                        .uploadAndDeleteAllFiles(snapshotDirectory, type, target, true)
+                        .forEach(future -> addCallback(future, snapshotDirectory));
 
                 // Next, upload secondary indexes
                 type = AbstractBackupPath.BackupFileType.SECONDARY_INDEX_V2;
@@ -426,5 +430,19 @@ public class SnapshotMetaTask extends AbstractBackup {
     @VisibleForTesting
     void setSnapshotName(String snapshotName) {
         this.snapshotName = snapshotName;
+    }
+
+    private static void addCallback(ListenableFuture<AbstractBackupPath> future, File snapshotDir) {
+        FutureCallback<AbstractBackupPath> callback =
+                new FutureCallback<AbstractBackupPath>() {
+                    @Override
+                    public void onSuccess(AbstractBackupPath result) {}
+
+                    @Override
+                    public void onFailure(Throwable t) {
+                        logger.error("Error uploading contents of snapshotDir {}", snapshotDir, t);
+                    }
+                };
+        Futures.addCallback(future, callback, MoreExecutors.directExecutor());
     }
 }

82dce0005fdb33fb24c6095d2a57d672268aaf4f
Matt Lehman <mattl@netflix.com>
2023-04-17 21:37:38 -0700

    Increment cross regional duplicate tokens to replicate the policy we have been applying manually. Throw when duplicate tokens are created in region because that would be an obvious error and we should not add two nodes in the same region so closely together.

--- priam/src/test/java/com/netflix/priam/config/FakeConfiguration.java
+++ priam/src/test/java/com/netflix/priam/config/FakeConfiguration.java
@@ -42,6 +42,7 @@ public class FakeConfiguration implements IConfiguration {
     private boolean skipIngressUnlessIPIsPublic;
     private long compressionTransitionEpochMillis;
     private boolean autoSnapshot;
+    private String partitioner;
 
     public Map<String, String> fakeProperties = new HashMap<>();
 
@@ -312,5 +313,14 @@ public class FakeConfiguration implements IConfiguration {
     @Override
     public boolean getAutoSnapshot() {
         return autoSnapshot;
+    }
+
+    public void setPartitioner(String partitioner) {
+        this.partitioner = partitioner;
+    }
+
+    @Override
+    public String getPartitioner() {
+        return partitioner;
     }
 }

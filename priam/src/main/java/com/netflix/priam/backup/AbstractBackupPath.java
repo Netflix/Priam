6b1027aa11305d866d071b08edd185effa8102f2
Matt Lehman <mattl@netflix.com>
2024-12-27 07:27:43 -0800

    Rename file variable to be idomatic Java.

--- priam/src/main/java/com/netflix/priam/backup/AbstractBackupPath.java
+++ priam/src/main/java/com/netflix/priam/backup/AbstractBackupPath.java
@@ -137,24 +137,24 @@ public abstract class AbstractBackupPath implements Comparable<AbstractBackupPat
 
     /** Local restore file */
     public File newRestoreFile() {
-        File return_;
+        File file;
         String dataDir = config.getRestoreDataLocation();
         switch (type) {
             case SECONDARY_INDEX_V2:
-                return_ =
+                file =
                         new File(
                                 PATH_JOINER.join(
                                         dataDir, keyspace, columnFamily, indexDir, fileName));
                 break;
             case META_V2:
-                return_ = new File(PATH_JOINER.join(config.getDataFileLocation(), fileName));
+                file = new File(PATH_JOINER.join(config.getDataFileLocation(), fileName));
                 break;
             default:
-                return_ = new File(PATH_JOINER.join(dataDir, keyspace, columnFamily, fileName));
+                file = new File(PATH_JOINER.join(dataDir, keyspace, columnFamily, fileName));
         }
-        File parent = new File(return_.getParent());
+        File parent = new File(file.getParent());
         if (!parent.exists()) parent.mkdirs();
-        return return_;
+        return file;
     }
 
     @Override
